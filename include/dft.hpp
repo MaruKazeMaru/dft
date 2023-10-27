7cff70a1739c2216a58a064c7bf6ca807b53b620
MaruKazeMaru <kaze47111@gmail.com>
2023-10-27 16:22:52 +0900

    initial commit

---
+++ include/dft.hpp
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: 2023 ShinagwaKazemaru
+// SPDX-License-Identifier: MIT License
+
+#ifndef __DFT_HPP__
+#define __DFT_HPP__
+
+#endif
---
+++ src/dft.cpp
@@ -0,0 +1,4 @@
+// SPDX-FileCopyrightText: 2023 ShinagwaKazemaru
+// SPDX-License-Identifier: MIT License
+
+#include <dft.hpp>
