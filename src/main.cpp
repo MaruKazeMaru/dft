3f92d249e019115c569032e8c9fcca94fe6ba9df
MaruKazeMaru <kaze47111@gmail.com>
2023-10-28 23:10:33 +0900

    testing dft

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 // SPDX-FileCopyrightText: 2023 ShinagwaKazemaru
 // SPDX-License-Identifier: MIT License
 
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+
 #include <dft.hpp>
 
 int main(int argc, char** argv){
-    
+    int n = 8;
+
+    float* f = new float[n];
+    for(int i = 0; i < n; ++i)
+        std::cin >> f[i];
+
+    float* f_hat = fft(n, f);
+
+    std::cout << "f_hat =" << std::endl;
+    for(int i = 0; i < n; ++i)
+        std::cout << f_hat[i] << std::endl;
+
+    delete f;
 }
