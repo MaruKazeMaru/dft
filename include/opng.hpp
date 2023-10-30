a34a9351b7cc450fa0bd329efa9b1a9d1a35476a
MaruKazeMaru <kaze47111@gmail.com>
2023-10-30 17:34:32 +0900

    fix include

--- include/opng.hpp
+++ include/opng.hpp
@@ -3,6 +3,8 @@
 #ifndef __OPNG_HPP__
 #define __OPNG_HPP__
 
+#include <iostream>
+
 class opng_write_error: public std::runtime_error{
     public:
     opng_write_error(const char* msg):runtime_error(msg){};
